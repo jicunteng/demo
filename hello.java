30f61f754ebfc42575e8bea358d109d863b3f071
ce <452863297@qq.com>
2019-12-05 15:21:17 +0800

    other write

---
+++ hello.java
@@ -0,0 +1,12 @@
+public List<CommStatisticsConfigForm> getOrg() {
+		String tenantId = TenantIdHolder.get();
+		StringBuilder sql = new StringBuilder()
+				.append("select o.item_code AS orgId,o.item_name AS orgName from hsp.hsp_config_baseinfo o ,\r\n"
+						+ "security.security_medical_vs_hsp p where o.item_code = p.hsp_code");
+		Map<String, Object> map = new HashMap<String, Object>();
+		if (StringUtils.isNotBlank(tenantId)) {
+			sql.append(" and p.medical_id=:tenantId ORDER BY o.seq_no ASC");
+			map.put("tenantId", tenantId.trim());
+		}
+		return (List<CommStatisticsConfigForm>) findListBeanBySql(sql.toString(), map, CommStatisticsConfigForm.class);
+	}
